eabde48ebf14a79ea572fdcb81701567136f7a65
yumlog <yumlog314@gmail.com>
2023-05-16 17:18:41 +0900

    .

---
+++ src/components/TodoItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./TodoItem.css";
+
+class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
+  
+  render() {
+    const { text, checked, id, color, onToggle, onRemove } = this.props;
+
+    return (
+      <div className="todo-item" onClick={() => onToggle(id)}>
+        <div className="remove" onClick={(e) => {
+          e.stopPropagation();
+          onRemove(id)}
+        }>&times;</div>
+        <div style={{ color }} className={`todo-text ${checked && 'checked'}`}>
+          <div>{text}</div>
+        </div>
+        {
+          checked && (<div className="check-mark">&#x2713;</div>)
+        }
+      </div>
+    )
+  }
+}
+
+export default TodoItem;
